4eef6944e165a900157f6651112db309c89b24d4
Harald Vinje <haraldvinje@gmail.com>
2025-05-04 14:45:39 +0200

    Update template

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -10,5 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1" 
+  region = "eu-west-1"
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "BUCKET_NAME" # Replace with your bucket name
-    key     = "terraform/terraform.tfstate"
-    region  = "eu-west-1"
-    encrypt = true
+    bucket       = "BUCKET_NAME" # Replace with your bucket name
+    key          = "terraform/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
